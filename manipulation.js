8cb4a46c95fbd43d23af61c97c037b04275076b9
SSAISNEHITH <saisnehith4509@gmail.com>
2026-06-12 11:57:39 +0530

    manipulation files

---
+++ manipulation.js
@@ -0,0 +1,17 @@
+
+//Querries
+
+let pen = document.getElementById("wish");
+console.log(pen);
+
+let pen1 = document.getElementsByClassName("card");
+console.log(pen1);
+
+let pen2 = document.getElementsByTagName("p");
+console.log(pen2);
+
+let pen3 = document.querySelector(".card");
+console.log(pen3);
+
+let pen4 = document.querySelectorAll(".card");
+console.log(pen4);
