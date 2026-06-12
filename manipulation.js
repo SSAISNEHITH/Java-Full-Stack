d937b91bc6189f6a9629815b94d3a4616ea5f2bd
SSAISNEHITH <saisnehith4509@gmail.com>
2026-06-12 20:42:46 +0530

    Changes and updates

--- manipulation.js
+++ manipulation.js
@@ -1,6 +1,6 @@
 
 //Querries
-
+/*
 let pen = document.getElementById("wish");
 console.log(pen);
 
@@ -15,3 +15,13 @@ console.log(pen3);
 
 let pen4 = document.querySelectorAll(".card");
 console.log(pen4);
+
+*/
+
+//
+let value=document.getElementById("wish").innerText="Happy Birthday Sai";
+console.log(value);
+//or another way to above 
+let value1=document.getElementById("wish");
+value1.innerText="Happy Birthday Sai";
+console.log(value1);
